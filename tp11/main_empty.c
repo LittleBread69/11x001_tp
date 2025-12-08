b76203abac391a1d9fa8f855cf825b0136de02f6
clemtarge <clementtarge@gmail.com>
2025-12-08 15:02:15 +0100

    Add TP11

---
+++ tp11/main_empty.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/****************************/
+/* Vos fonctions ci-dessous */ 
+/****************************/
+
+// EXERCICE 2
+
+// EXERCICE 3
+
+
+/****************************/
+/* Vos fonctions ci-dessus **/
+/****************************/
+
+void exercice1(void) {
+    printf("\n\nEXERCICE 1\n\n");
+
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+void exercice2(void) {
+    printf("\n\nEXERCICE 2\n\n");
+
+    int arr[] = {9, 19, 8, 3, 7, 11, 0, 22, 14, 3};
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+void exercice3(void) {
+    printf("\n\nEXERCICE 3\n\n");
+
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+int main(void) { 
+    exercice1();
+    exercice2();
+    exercice3();
+    return 0;
+}
+
